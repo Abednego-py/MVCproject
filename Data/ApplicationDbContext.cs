8419229715b3f5974b04cb823361d6ac10ce1ae1
Abednego-py <abednegoaginam@gmail.com>
2022-06-14 13:18:50 +0100

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCproject.Models;
+
+namespace MVCproject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVCproject.Models.Joke>? Joke { get; set; }
+    }
+}
